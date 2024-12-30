30d350fd5872d8b6322930e2fae7ddc85e2d81d5
ashesh <ashesh7272@gmail.com>
2024-12-30 10:49:23 +0545

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/header/Header";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div>
       <Header />
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <a href="#menu">Menu</a>
+      <a href="#special">Special</a>
+      <a href="#aboutus">About US</a>
+      <a href="#contactus">Contact us</a>
+    </nav>
+  );
+};
+
+export default Navbar;
